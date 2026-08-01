1028e587151a7443f8120d6b6bc7bb38ef100095
ashishkumar1529 <ashishchalotra7@gmail.com>
2026-08-01 23:02:08 +0530

    Delete classroom directory

--- classroom/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app =express(); 
-const session = require("express-sessions");
-
-app.listen(3000, () => {
-    console.log("server is listening to the port");
-})
